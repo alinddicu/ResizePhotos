e6c928366183ca4dfe0c7a0c163493e07ab49c1b
alinddicu <alinddicu@yahoo.com>
2014-08-16 14:57:38 +0200

    Init GIT

---
+++ ResizePhotos/ResizePhotos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Drawing;
+
+namespace ResizePhotos
+{
+    class Program
+    {
+        //private const string SRC_DIR = @"C:\Temp\photos\source\";
+        //private const string DEST_DIR = @"C:\Temp\photos\dest\";
+        private const string SRC_DIR = @"C:\Temp\test\source\";
+        private const string DEST_DIR = @"C:\Temp\test\dest\";
+        private const int DEST_WIDTH = 32;
+        private const int DEST_HEIGHT = 32;
+
+        static void Main(string[] args)
+        {
+            string[] filePaths = Directory.GetFiles(SRC_DIR, "*.PNG", SearchOption.AllDirectories);
+
+            Resizer resizer = new Resizer();
+            Image bmPhoto = null;
+
+            foreach (string file in filePaths)
+            {
+                bmPhoto = resizer.ResizeImage(DEST_WIDTH, DEST_HEIGHT, file);
+
+                string name = file.Replace("source", "dest").Replace(Path.GetExtension(file), ".PNG");
+
+                bmPhoto.Save(name);
+            }
+
+            // del /s *.JPG
+        }
+    }
+}
---
+++ ResizePhotos/ResizePhotos/Resizer.cs
@@ -0,0 +1,58 @@
+﻿namespace ResizePhotos
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Drawing.Imaging;
+    using System.Drawing.Drawing2D;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    public class Resizer
+    {
+        public Image ResizeImage(int newWidth, int newHeight, string stPhotoPath)
+        {
+            Image imgPhoto = Image.FromFile(stPhotoPath);
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+
+            float nPercent = 0;
+            
+            int calcWidth = newWidth;
+            int calcHeight = newHeight;
+
+            nPercent = ((float)calcWidth / (float)sourceWidth);
+            //Consider vertical pics
+            if (sourceWidth < sourceHeight)
+            {
+                int buff = calcWidth;
+
+                calcWidth = calcHeight;
+                calcHeight = buff;
+
+                nPercent = ((float)calcHeight / (float)sourceHeight);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+            Bitmap bmPhoto = new Bitmap(destWidth, destHeight, PixelFormat.Format24bppRgb);
+
+            bmPhoto.SetResolution(imgPhoto.HorizontalResolution, imgPhoto.VerticalResolution);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.Black);
+            grPhoto.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto, new Rectangle(0, 0, destWidth, destHeight), new Rectangle(0, 0, sourceWidth, sourceHeight), GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            imgPhoto.Dispose();
+            return bmPhoto;
+        }
+    }
+}
